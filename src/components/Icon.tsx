1efcbdd6ee504bb5202b8650d206980f1445613e
Allen <1942813644@qq.com>
2022-03-22 17:20:08 +0800

    大屏可视化项目

---
+++ src/components/Icon.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export const Icon = (props: { name: string }) => {
+  return (
+    <svg className="icon">
+      <use xlinkHref={'#icon-' + props.name}/>
+    </svg>
+  );
+};
---
+++ src/shared/px.ts
@@ -0,0 +1,2 @@
+
+export const px = (n) => n / 800 * (window as any).pageWidth;
